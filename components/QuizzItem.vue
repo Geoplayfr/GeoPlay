c99f346b3e0d344f713663dc3f0160a9a3e9b9e5
Marc <marc.guiocheau@efrei.net>
2020-11-22 23:15:20 +0100

    NOD-22 #Comment added Homepage with a QuizzItem component

---
+++ components/QuizzItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card :loading="loading" fluid>
+      <v-card-title>{{ quizz.name }}</v-card-title>
+      <v-card-text
+        >Diffculty: {{ quizz.difficulty }} <br />Duration: {{ quizz.duration }}
+        <br />Number of questions: {{ quizz.nbQuestion }}<br />Creator:
+        {{ quizz.creator }}
+      </v-card-text>
+      <v-card-actions v>
+        <v-btn color="primary">Start</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    quizz: {
+      required: true,
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ pages/homepage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <v-list-item-content v-for="quizz in quizzes" :key="quizz.id">
+      <quizz-item
+        :quizz="quizz"
+      />
+    </v-list-item-content>
+  </div>
+</template>
+<script>
+import QuizzItem from "../components/QuizzItem.vue";
+export default {
+  layout: "default",
+  components: {
+    QuizzItem,
+  },
+  data() {
+    return {
+      errorMsg: "",
+      snackbar: false,
+      username: "",
+      password: "",
+      show1: false,
+      valid: false,
+      title: "Hello word!",
+      quizzes: [
+        {
+          id: 1,
+          name: "This is a tittle",
+          mapid: 1,
+          difficulty: "easy",
+          duration: 10,
+          creator: "Franck94",
+          nbQuestion: 3,
+        },  
+        {
+          id: 2,
+          name: "This is a tittle",
+          mapid: 1,
+          difficulty: "easy",
+          duration: 10,
+          creator: "Franck94",
+          nbQuestion: 3,
+        },
+         {
+          id: 1,
+          name: "This is a tittle",
+          mapid: 1,
+          difficulty: "easy",
+          duration: 10,
+          creator: "Franck94",
+          nbQuestion: 3,
+        },  
+        {
+          id: 2,
+          name: "This is a tittle",
+          mapid: 1,
+          difficulty: "easy",
+          duration: 10,
+          creator: "Franck94",
+          nbQuestion: 3,
+        },
+         {
+          id: 1,
+          name: "This is a tittle",
+          mapid: 1,
+          difficulty: "easy",
+          duration: 10,
+          creator: "Franck94",
+          nbQuestion: 3,
+        },  
+        {
+          id: 2,
+          name: "This is a tittle",
+          mapid: 1,
+          difficulty: "easy",
+          duration: 10,
+          creator: "Franck94",
+          nbQuestion: 3,
+        }
+      ],
+    };
+  },
+};
+</script>
