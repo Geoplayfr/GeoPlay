4541edcbd43f2da684586adb95442688625ada1d
Marc <marc.guiocheau@efrei.net>
2020-11-25 18:57:02 +0100

    NOD-22 #Comment responsive

--- components/QuizzItem.vue
+++ components/QuizzItem.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <v-card :loading="loading" fluid>
-      <v-card-title>{{ quizz.name }}</v-card-title>
-      <v-card-text
-        >Diffculty: {{ quizz.difficulty }} <br />Duration: {{ quizz.duration }}
-        <br />Number of questions: {{ quizz.nbQuestion }}<br />Creator:
-        {{ quizz.creator }}
-      </v-card-text>
-      <v-card-actions v>
-        <v-btn color="primary">Start</v-btn>
-      </v-card-actions>
+      <v-row>
+        <v-col cols="10">
+          <v-card-title>{{ quizz.name }}</v-card-title>
+          <v-card-text
+            >Diffculty: {{ quizz.difficulty }} <br />Duration:
+            {{ quizz.duration }} <br />Number of questions: {{ quizz.nbQuestion
+            }}<br />Creator:
+            {{ quizz.creator }}
+          </v-card-text>
+        </v-col>
+        <v-col align-self="center">
+          <v-card-actions>
+            <v-btn color="primary">Start</v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
     </v-card>
   </div>
 </template>
