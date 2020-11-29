639dfe97d1d79f3cf68bbe88c9f2c8862922b748
Marc <marc.guiocheau@efrei.net>
2020-11-29 16:16:55 +0100

    NOD-22 #Comment responsive

--- components/QuizzItem.vue
+++ components/QuizzItem.vue
@@ -4,14 +4,33 @@
       <v-row>
         <v-col cols="10">
           <v-card-title>{{ quizz.name }}</v-card-title>
-          <v-card-text
-            >Diffculty: {{ quizz.difficulty }} <br />Duration:
-            {{ quizz.duration }} <br />Number of questions: {{ quizz.nbQuestion
-            }}<br />Creator:
-            {{ quizz.creator }}
-          </v-card-text>
+          <v-card-text class="pt-0 pb-0"
+            >Description: {{ quizz.description }}</v-card-text
+          >
+          <v-row> 
+            <v-col
+              ><v-card-text class="pb-0"
+                >Difficulty: {{ quizz.difficulty }}</v-card-text
+              ></v-col
+            >
+            <v-col
+              ><v-card-text class="pb-0" 
+                >Duration: {{ quizz.duration }} sec</v-card-text
+              ></v-col
+            >
+            <v-col>
+              <v-card-text class="pb-0"
+                >{{ quizz.nbQuestion }} questions</v-card-text
+              ></v-col
+            >
+            <v-col
+              ><v-card-text class="pb-0"
+                >Creator: {{ quizz.creator }}</v-card-text
+              ></v-col
+            >
+          </v-row>
         </v-col>
-        <v-col align-self="center">
+        <v-col class="d-flex align-end flex-column mt-auto">
           <v-card-actions>
             <v-btn color="primary">Start</v-btn>
           </v-card-actions>
