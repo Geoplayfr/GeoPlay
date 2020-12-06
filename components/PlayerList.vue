5d2c9b04ebc3bce0184cdb8791603bc307266b40
Frank <francois.gillioen@efrei.net>
2020-12-06 16:06:38 +0100

    NOD-32 #Comment Quick player list draft

---
+++ components/PlayerList.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-list>
+    <v-list-item-avatar class="mx-3">
+      <v-icon dark>mdi-account-circle</v-icon>
+    </v-list-item-avatar>
+    <v-list-item-avatar v-for="p in playerList" :key="p.username">
+      <v-icon dark>mdi-account-circle</v-icon>
+    </v-list-item-avatar>
+  </v-list>
+</template>
+<script>
+export default {
+  props: {
+    playerList: {
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
