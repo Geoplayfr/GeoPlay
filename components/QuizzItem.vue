6fb8607e182d5969739dc99fa1ca57feff932da2
Nicolas <nicolas.guibert@efrei.net>
2020-12-10 18:39:14 +0100

    NOD-31 #Comment Add ranking page

--- components/QuizzItem.vue
+++ components/QuizzItem.vue
@@ -2,8 +2,18 @@
   <div>
     <v-card fluid>
       <v-row>
-        <v-col cols="10">
-          <v-card-title>{{ quizz.name }}</v-card-title>
+        <v-col>
+          <v-card-title style="padding: 0px 15px">{{ quizz.name }}
+            <v-col class="text-right">
+            <v-btn 
+              color="primary"
+              nuxt
+              icon
+              :to="{ name: 'ranking', params: { id_quiz: quizz.id_quiz, nb_questions: quizz.nb_questions, quiz_name: quizz.name} }">
+                <v-icon>mdi-trophy</v-icon>
+            </v-btn>
+            </v-col>
+          </v-card-title>
           <v-card-text class="pt-0 pb-0"
             >Description: {{ quizz.description }}</v-card-text
           >
@@ -28,24 +38,15 @@
                 >Creator: {{ quizz.creator }}</v-card-text
               ></v-col
             >
-          </v-row>
-        </v-col>
-        <v-col class="d-flex align-end flex-column mt-auto">
-          <v-card-actions>
-            <v-btn 
+            <v-col class="text-right" style="margin:10px">
+              <v-btn 
               color="primary"
               nuxt
               :to="{ name: 'game', params: { id_quiz: quizz.id_quiz} }">
-                Start
-            </v-btn>
-            <v-btn 
-              color="primary"
-              nuxt
-              icon
-              :to="{ name: 'ladder', params: { id_quiz: quizz.id_quiz, nb_questions: quizz.nb_questions} }">
-                <v-icon>mdi-trophy</v-icon>
-            </v-btn>
-          </v-card-actions>
+                Start {{ quizz.id_quiz }}
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-card>
