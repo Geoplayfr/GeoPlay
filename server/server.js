2726d6af8592b9cdc850b482f7c8325d6e07faef
Frank <francois.gillioen@efrei.net>
2020-12-16 01:33:50 +0100

    deploy tweak

--- server/server.js
+++ server/server.js
@@ -1,38 +1,23 @@
+import express from 'express'
 import session from 'express-session'
 import logger from 'morgan'
-import express from 'express'
 import apiRouter from './routes/routes.js'
 import config from './server.config.js'
 import postgresStore from './postgres-store.js'
-const app = express()
-const { Nuxt, Builder } = require('nuxt')
-const http = require('http').createServer(app)
 
 postgresStore.init(config.postgres)
-config.dev = process.env.NODE_ENV !== 'production'
 
-async function start () {
-  const nuxt = new Nuxt(config)
-  // Start build process in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    builder.build()
-  }
-  app.use(nuxt.render)
-  const port = process.env.PORT
-  http.listen(port, '0.0.0.0')
-  console.log('Server listening on localhost:' + port) // eslint-disable-line no-console
+const app = express()
 
-  app.use(logger('dev'))
-  app.use(session({
-    secret: config.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
+app.use(logger('dev'))
+app.use(session({
+  secret: config.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-  app.use('/api/', apiRouter)
-}
-start()
+app.use('/api/', apiRouter)
+
 export default app
