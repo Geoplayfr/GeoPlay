9ef142d0de5f77d12556b3fa11b712e48ddd2d59
Frank <francois.gillioen@efrei.net>
2021-01-14 02:12:12 +0100

    database verification error message

--- server/server.js
+++ server/server.js
@@ -5,19 +5,22 @@ import apiRouter from './routes/routes.js'
 import config from './server.config.js'
 import postgresStore from './postgres-store.js'
 
-postgresStore.init(config.postgres)
+export default async function run () {
+  await postgresStore.init(config.postgres)
+  // const queryString = 'SELECT EXISTS (SELECT 1 FROM   information_schema.tables WHERE  table_name = "users");'
 
-const app = express()
+  const app = express()
 
-app.use(logger('dev'))
-app.use(session({
-  secret: config.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+  app.use(logger('dev'))
+  app.use(session({
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/', apiRouter)
-
-export default app
+  app.use('/api/', apiRouter)
+  return app
+}
+run()
